c48f4fce30a00416f94440430db41c66201d6645
Porntip Aramrattana <sakuracute26@gmail.com>
2013-09-22 02:22:08 +0700

    Modified ws

--- ws/src/main/java/com/egt/ws/controller/JSONController.java
+++ ws/src/main/java/com/egt/ws/controller/JSONController.java
@@ -1,16 +1,30 @@
 package com.egt.ws.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.egt.persistence.config.PersistenceJPAConfigXml;
+import com.egt.persistence.services.UserService;
 import com.egt.ws.model.Shop;
 
 @Controller
+@EnableTransactionManagement
+@ComponentScan({ "com.egt.persistence" })
+@ImportResource({ "*persistence-context.xml" })
 public class JSONController {
-
+	@Autowired
+	public UserService service;
+	
+	
 	@RequestMapping(value = "/egt/{name}", method = RequestMethod.GET, 
 			headers="Accept=application/json")
 	@ResponseBody
